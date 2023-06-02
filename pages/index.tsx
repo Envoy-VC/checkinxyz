6b5369643d36d3cd920714300c0b55ddbcd6e3ce
Vedant Chainani <vedantchainani1084@gmail.com>
2023-06-02 19:47:21 +0530

    add cover section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import { Inter } from 'next/font/google';
 
+import { Cover } from '@/sections';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
 	return (
-		<main>
-			<div className='text-3xl'>Hello</div>
+		<main className='p-8'>
+			<div className='max-w-screen-xl mx-auto rounded-[48px]'>
+				<div className='flex flex-col lg:flex-row gap-6'>
+					<div className='basis-1/2 order-2 lg:order-1 shadow-lg rounded-[48px]'>
+						Hello
+					</div>
+					<div className='basis-1/2 order-1 lg:order-2'>
+						<Cover />
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 }
---
+++ sections/cover/index.tsx
@@ -0,0 +1,23 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { Image } from '@nextui-org/react';
+
+import coverImage from '@/assets/cover.jpg';
+
+const Cover = () => {
+	return (
+		<div className='h-full min-h-[800px] relative'>
+			<div className='absolute z-[10000] flex justify-end pr-8 pt-4 w-full'>
+				<ConnectButton />
+			</div>
+
+			<Image
+				src={coverImage.src}
+				alt='Astronaut'
+				className='!h-full rounded-[48px]'
+				objectFit='cover'
+			/>
+		</div>
+	);
+};
+
+export default Cover;
