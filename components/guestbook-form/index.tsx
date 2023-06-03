d67c2cb530a20cdc7e7a28f44172a583fc8a6175
Vedant Chainani <vedantchainani1084@gmail.com>
2023-06-03 19:33:05 +0530

    Add contract call logic and react toast

--- components/guestbook-form/index.tsx
+++ components/guestbook-form/index.tsx
@@ -1,7 +1,16 @@
-import { ReactEventHandler, useState } from 'react';
-import { Input, Textarea, Button } from '@nextui-org/react';
+import { useState } from 'react';
+import { Input, Textarea, Button, Loading } from '@nextui-org/react';
+import {
+	useContract,
+	useContractWrite,
+	useStorage,
+	useAddress,
+} from '@thirdweb-dev/react';
+
+import toast, { Toaster } from 'react-hot-toast';
 
 import { Inter } from 'next/font/google';
+import { GUESTBOOK_CONTRACT_ADDRESS, ABI } from '@/utils/config';
 const inter = Inter({ subsets: ['latin'] });
 
 interface FormProps {
@@ -14,9 +23,47 @@ const GuestbookForm = () => {
 		name: '',
 		message: '',
 	});
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const address = useAddress();
+	const storage = useStorage();
+
+	const { contract, isLoading: isInitialized } = useContract(
+		GUESTBOOK_CONTRACT_ADDRESS,
+		ABI
+	);
+
+	const { mutateAsync: writeAsync } = useContractWrite(contract, 'createEntry');
 
 	function handleFormChange(fieldName: string, value: string) {
 		setForm({ ...form, [fieldName]: value });
+	}
+
+	async function handleSubmit(form: FormProps) {
+		try {
+			setIsLoading(true);
+			if (!address) {
+				toast.error('Please Connect Wallet');
+				throw new Error('No address found');
+			}
+			const timestamp = Math.floor(Date.now() / 1000);
+			const metadata = {
+				name: form.name,
+				message: form.message,
+				timestamp: timestamp,
+			};
+			const uri = await storage?.upload(metadata, {
+				alwaysUpload: false,
+				uploadWithoutDirectory: true,
+			});
+			await writeAsync({
+				args: [uri!.split('ipfs://')[1]],
+			});
+			toast.success('Success!');
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
 	}
 
 	return (
@@ -34,17 +81,19 @@ const GuestbookForm = () => {
 			<div>
 				<form
 					className='flex flex-col gap-6 mt-12'
-					onSubmit={(e) => {
+					onSubmit={async (e) => {
 						e.preventDefault();
-						console.log(form);
+						await handleSubmit(form);
 					}}
 				>
 					<Input
+						aria-label='Name'
 						required={true}
 						placeholder='Name'
 						onChange={(e) => handleFormChange('name', e.target.value)}
 					/>
 					<Textarea
+						aria-label='Message'
 						required={true}
 						placeholder='Enter your amazing ideas.'
 						minRows={4}
@@ -55,11 +104,17 @@ const GuestbookForm = () => {
 						size='lg'
 						type='submit'
 						className='text-white bg-[#9750DD] !w-fit mx-auto mt-16'
+						disabled={isInitialized}
 					>
-						Sign
+						{isInitialized || isLoading ? (
+							<Loading type='points' color='currentColor' size='sm' />
+						) : (
+							'Sign'
+						)}
 					</Button>
 				</form>
 			</div>
+			<Toaster position='bottom-left' />
 		</div>
 	);
 };
--- utils/config.ts
+++ utils/config.ts
@@ -10,3 +10,110 @@ export const PAPER_API_KEY = process.env.NEXT_PUBLIC_PAPER_API_KEY || '';
 
 export const GUESTBOOK_CONTRACT_ADDRESS =
 	'0x6Bf13132D8E8f74F3B9bA253B15B3f88EdDD6942';
+
+export const ABI = [
+	{
+		inputs: [
+			{
+				internalType: 'string',
+				name: '_uri',
+				type: 'string',
+			},
+		],
+		name: 'createEntry',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'deleteGuesbook',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'previousOwner',
+				type: 'address',
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'newOwner',
+				type: 'address',
+			},
+		],
+		name: 'OwnershipTransferred',
+		type: 'event',
+	},
+	{
+		inputs: [],
+		name: 'renounceOwnership',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'newOwner',
+				type: 'address',
+			},
+		],
+		name: 'transferOwnership',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		name: 'entries',
+		outputs: [
+			{
+				internalType: 'string',
+				name: '',
+				type: 'string',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'getEntries',
+		outputs: [
+			{
+				internalType: 'string[]',
+				name: '',
+				type: 'string[]',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'owner',
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+];
